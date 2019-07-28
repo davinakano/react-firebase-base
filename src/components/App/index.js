c6e33b6315eb41c1c027fb01c53c19adb18b0b4c
Davi Nakano <davinotdavid@gmail.com>
2019-07-27 17:28:17 -0700

    Init with crapp and react router

---
+++ src/components/App/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigation from '../Navigation';
+
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => {
+  return (
+    <Router>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage}/>
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+    </Router>
+  )
+}
+
+export default App;
